e8732f289ad8c6a7f339041b6b9b4679919f5258
Sergio David Ramos Uni <sergiodhavidramos20@gmail.com>
2023-01-27 16:50:46 -0500

    Inicial Proyecto

---
+++ calculadorasimplex/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CalculadorasimplexConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "calculadorasimplex"
---
+++ calculadorasimplex/js/index.js
@@ -0,0 +1,5 @@
+const btngenerar = document.getElementById('btngenerar');
+
+btngenerar.addEventListener('click', ()=>{
+    alert('Presiono')
+})
